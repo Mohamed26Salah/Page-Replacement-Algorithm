676775f284d594e7a8aca0b01d5ccc0549c2f408
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2021-12-30 16:51:00 +0200

    small update 3

--- PageReplacement/PageReplacement/PageReplacement.cpp
+++ PageReplacement/PageReplacement/PageReplacement.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 using namespace std;
 
-int findLeastRecentlyUsed(int time[], int num) {
-	int g, min = time[0], pos = 0;
+int findLeastRecentlyUsed(int array[], int num) {
+	int g, min = array[0], position = 0;
 
 	for (g = 1; g < num; ++g) {
 
-		if (time[g] < min) {
-			min = time[g];
-			pos = g;
+		if (array[g] < min) {
+			min = array[g];
+			position = g;
 		}
 
 	}
-	return pos;
+	return position;
 }
 int main()
 {
@@ -23,38 +23,38 @@ int main()
     cin >> name;
     if (name == "FIFO") {
 		
-		int i, j, Npage, ref_str[50], frame[10], Nframe, k, free, fcount = 0;
+		int ll, hh, Npage, referenceStr[50], framesArray[10], Nframe, uu, free, framecount = 0;
 		
-		cout << " ENTER THE NUMBER OF PAGES:" << endl;
+		cout << "Enter the number of pages you want: " << endl;
 		cin >> Npage;
 		
-		cout << "\n ENTER THE PAGE NUMBER :" << endl;
-		for (i = 1; i <= Npage; i++)
-			cin >> ref_str[i];
+		cout << "\nEnter the reference String:" << endl;
+		for (ll = 1; ll <= Npage; ll++)
+			cin >> referenceStr[ll];
 	
-		cout << "\n ENTER THE NUMBER OF FRAMES :" << endl;
+		cout << "\n Enter the number of frames you want :" << endl;
 		cin >> Nframe;
 		// 7ot kolo belnegative
-		for (i = 0; i < Nframe; i++)
-			frame[i] = -1;
+		for (ll = 0; ll < Nframe; ll++)
+			framesArray[ll] = -1;
 		
-		j = 0;
-		cout << "\nreference string" << endl;
-		for (i = 1; i <= Npage; i++)
+		hh = 0;
+		cout << "\nreference String" << endl;
+		for (ll = 1; ll <= Npage; ll++)
 		{
-			cout << ref_str[i] << "\t\t";
+			cout << referenceStr[ll] << "\t\t";
 			
 			free = 0;
-			for (k = 0; k < Nframe; k++)
+			for (uu = 0; uu < Nframe; uu++)
 
 				// byshofo law homa shbah b3d
-				if (frame[k] == ref_str[i])
+				if (framesArray[uu] == referenceStr[ll])
 				{
 					
 					free = 1;
-					for (k = 0; k < Nframe; k++)
+					for (uu = 0; uu < Nframe; uu++)
 						
-						cout << frame[k] << "\t";
+						cout << framesArray[uu] << "\t";
 					
 					cout << "HIT";
 				}
@@ -62,61 +62,63 @@ int main()
 			if (free == 0)
 			{
 				
-				frame[j] = ref_str[i];
+				framesArray[hh] = referenceStr[ll];
 				
-				j = (j + 1) % Nframe;
+				hh = (hh + 1) % Nframe;
 				
-				fcount++;
-				for (k = 0; k < Nframe; k++)
+				framecount++;
+				for (uu = 0; uu < Nframe; uu++)
 					// Print elframes
-					cout << frame[k] << "\t";
+					cout << framesArray[uu] << "\t";
 				
 				cout << "FAULT";
 			}
 			cout << "\n";
 		}
-		cout << "Page Fault Is : " << fcount;
+		cout << "\n\nThe number of page faults = " << framecount;
 		return 0;
-    }
+    }////////////////////////////////////////////////////
     else if (name == "LRU") {
-        int frame_num, page_num, frames[20], pages[50], counter = 0, time[20], flag_1, flag_2, i, j, pos, faults = 0, hit = 0;
-        cout << "Enter the number of pages: ";
+        int frame_num, page_num, framesArray[20], pagesArray[50], counter = 0, time[20], flag_1, flag_2, ii, jj, pos, faults = 0, numOfhits = 0;
+        cout << "Enter the number of pages you want: ";
         cin >> page_num;
 
-        cout << "Enter the reference string: ";
-        for (i = 0; i < page_num; ++i) {
-            cin >> pages[i];
+        cout << "\nEnter the reference String: ";
+        for (ii = 0; ii < page_num; ++ii) {
+            cin >> pagesArray[ii];
         }
 
-        cout << "Enter the number of frames: ";
+        cout << "\n Enter the number of frames you want :";
         cin >> frame_num;
 
         //3by kolo be -1
-        for (i = 0; i < frame_num; ++i) {
-            frames[i] = -1;
+        for (ii = 0; ii < frame_num; ++ii) {
+			framesArray[ii] = -1;
         }
         //btlf 7waleen el reference string
-        for (i = 0; i < page_num; ++i) {
+        for (ii = 0; ii < page_num; ++ii) {
             flag_1 = flag_2 = 0;
             //btshoof fe 7ad shbhaa wla l2
-            for (j = 0; j < frame_num; ++j) {
-                if (frames[j] == pages[i]) {
-                    counter++;
-                    time[j] = counter;
+            for (jj = 0; jj < frame_num; ++jj) {
+                if (framesArray[jj] == pagesArray[ii]) {
+              
+                    time[jj] = counter;
+					counter++;
+					numOfhits++;
                     flag_1 = flag_2 = 1;
-                    hit++;
+					
                     break;
                 }
 
             }
             //btshoof fe negative one wla l2
             if (flag_1 == 0) {
-                for (j = 0; j < frame_num; ++j) {
-                    if (frames[j] == -1) {
+                for (jj = 0; jj < frame_num; ++jj) {
+                    if (framesArray[jj] == -1) {
                         counter++;
                         faults++;
-                        frames[j] = pages[i];
-                        time[j] = counter;
+						framesArray[jj] = pagesArray[ii];
+                        time[jj] = counter;
                         flag_2 = 1;
                         break;
                     }
@@ -125,106 +127,113 @@ int main()
             //btshoof meen elvictim ely hytbdl
             if (flag_2 == 0) {
                 pos = findLeastRecentlyUsed(time, frame_num);
+				framesArray[pos] = pagesArray[ii];
+
                 counter++;
-                faults++;
-                frames[pos] = pages[i];
+               
                 time[pos] = counter;
+				faults++;
             }
 
             cout << "\n";
             // display the frames
-            for (j = 0; j < frame_num; ++j) {
-                cout << frames[j] << "\t";
+            for (jj = 0; jj < frame_num; ++jj) {
+                cout << framesArray[jj] << "\t";
             }
         }
-        cout << "\n\nTotal Page Faults = " << faults;
-        cout << "\nTotal number of hits = " << hit;
+        cout << "\n\nThe number of page faults = " << faults;
+        cout << "\nThe number of hits = " << numOfhits;
         return 0;
-    }
+    }/////////////////////////////////////////////////////////////////////////////////////////
     else if (name == "OPTIMAL") {
-	int frame_num, page_num, frames[10], pages[30], hit = 0, temp[10], flag1, flag2, flag3, i, j, k, pos, max, faults = 0;
-	cout << "Enter number of pages: ";
+	int frame_num, page_num, ii, jj, kk, framesArray[10], pagesArray[30], hit = 0, temp[10], flag_1, flag_2, flag_3, position, max, numOffaults = 0;
+	cout << "Enter the number of pages you want: ";
 	cin >> page_num;
-	cout << "Enter reference string: ";
-	for (i = 0; i < page_num; ++i) {
-		cin >> pages[i];
+	cout << "\nEnter the reference String: ";
+	for (ii = 0; ii < page_num; ++ii) {
+		cin >> pagesArray[ii];
 	}
-	cout << "Enter number of frames: ";
+	cout << "\n Enter the number of frames you want :";
 	cin >> frame_num;
 	//bt7ot kolo bel negative
-	for (i = 0; i < frame_num; ++i) {
-		frames[i] = -1;
+	for (ii = 0; ii < frame_num; ++ii) {
+		framesArray[ii] = -1;
 	}
 	//btshoof el7agat elshbah b3d
-	for (i = 0; i < page_num; ++i) {
-		flag1 = flag2 = 0;
+	for (ii = 0; ii < page_num; ++ii) {
+		flag_1 = flag_2 = 0;
 
-		for (j = 0; j < frame_num; ++j) {
-			if (frames[j] == pages[i]) {
-				flag1 = flag2 = 1;
+		for (jj = 0; jj < frame_num; ++jj) {
+			if (framesArray[jj] == pagesArray[ii]) {
+				flag_1 = flag_2 = 1;
 				hit++;
 				break;
 			}
 		}
 		//btshoof meen belnegative
-		if (flag1 == 0) {
-			for (j = 0; j < frame_num; ++j) {
-				if (frames[j] == -1) {
-					faults++;
-					frames[j] = pages[i];
-					flag2 = 1;
+		if (flag_1 == 0) {
+			for (jj = 0; jj < frame_num; ++jj) {
+				if (framesArray[jj] == -1) {
+					
+					framesArray[jj] = pagesArray[ii];
+					numOffaults++;
+					flag_2 = 1;
+
 					break;
 				}
 			}
 		}
 		//optimal
-		if (flag2 == 0) {
-			flag3 = 0;
-			for (j = 0; j < frame_num; ++j) {
-				temp[j] = -1;
-				for (k = i + 1; k < page_num; ++k) {
-					if (frames[j] == pages[k])
+		if (flag_2 == 0) {
+			flag_3 = 0;
+			for (jj = 0; jj < frame_num; ++jj) {
+				temp[jj] = -1;
+				for (kk = ii + 1; kk < page_num; ++kk) {
+					if (framesArray[jj] == pagesArray[kk])
 					{
-						temp[j] = k;
+						temp[jj] = kk;
 						break;
 					}
 				}
 			}
 			//fcfs 
-			for (j = 0; j < frame_num; ++j) {
-				if (temp[j] == -1)
+			for (jj = 0; jj < frame_num; ++jj) {
+				if (temp[jj] == -1)
 				{
-					pos = j;
-					flag3 = 1;
+					flag_3 = 1;
+
+					position = jj;
+					
 					break;
 				}
 
 			}
-			if (flag3 == 0)
+			if (flag_3 == 0)
 			{
 				max = temp[0];
-				pos = 0;
-				for (j = 1; j < frame_num; ++j)
+				position = 0;
+				for (jj = 1; jj < frame_num; ++jj)
 				{
-					if (temp[j] > max) {
-						max = temp[j];
-						pos = j;
+					if (temp[jj] > max) {
+						max = temp[jj];
+						position = jj;
 
 					}
 				}
 			}
-			frames[pos] = pages[i];
-			faults++;
+			numOffaults++;
+			framesArray[position] = pagesArray[ii];
+			
 		}
 
 		cout << "\n";
 
-		for (j = 0; j < frame_num; ++j) {
-			cout << frames[j] << "\t";
+		for (jj = 0; jj < frame_num; ++jj) {
+			cout << framesArray[jj] << "\t";
 		}
 	}
-	cout << "\n\nTotal Page Faults = " << faults;
-	cout << "\n The number of hits = " << hit;
+	cout << "\n\nThe number of page faults = " << numOffaults;
+	cout << "\nThe number of hits = " << hit;
 	return 0;
     }
     else {
